d87d57360ba2490eb7a4c28bedb74afb72b05305
RDDcat <yukmaro@gmail.com>
2023-03-19 23:32:44 +0900

    [UPDATE] user-service 로그인 기능 추가

--- user-service/src/main/java/com/maro/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/maro/userservice/security/WebSecurity.java
@@ -1,17 +1,55 @@
 package com.maro.userservice.security;
 
+import com.maro.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    UserService userService;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    Environment env;
+
+    @Autowired
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment env) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.env = env;
+    }
+
+
+    // 권한
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("1270.0.0.1")
+                .and()
+                .addFilter(getAuthentication());
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthentication() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    // 인증
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- user-service/src/main/java/com/maro/userservice/service/UserService.java
+++ user-service/src/main/java/com/maro/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package com.maro.userservice.service;
 
 import com.maro.userservice.dto.UserDTO;
 import com.maro.userservice.repository.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDTO createUser(UserDTO userDTO);
     UserDTO getUserByUserId(String userId);
     Iterable<UserEntity> getUserByAll();
