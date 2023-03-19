02b2b708ae7bdddf20fd2b2cce93a644c71e05f2
RDDcat <yukmaro@gmail.com>
2023-03-20 00:10:01 +0900

    [FIX] 에러 해결 (정상실행)

--- user-service/src/main/java/com/maro/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/maro/userservice/security/WebSecurity.java
@@ -34,7 +34,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress("1270.0.0.1")
+                .hasIpAddress("127.0.0.1")
                 .and()
                 .addFilter(getAuthentication());
         http.headers().frameOptions().disable();
