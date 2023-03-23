94b0356c41de18b8c0958bab7d569f154f4679a4
RDDcat <yukmaro@gmail.com>
2023-03-23 17:04:32 +0900

    [FIX] 버그 픽스 게터 세터가 없어서 접근이 안됐음

--- order-service/src/main/java/com/maro/orderservice/dto/KafkaOrderDTO.java
+++ order-service/src/main/java/com/maro/orderservice/dto/KafkaOrderDTO.java
@@ -1,9 +1,11 @@
 package com.maro.orderservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
 @AllArgsConstructor
 public class KafkaOrderDTO implements Serializable {
     private Schema schema;
