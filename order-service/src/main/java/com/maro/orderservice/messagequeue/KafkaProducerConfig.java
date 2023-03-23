b3a04c76296607d9b772d6b589dd9cbfc7ee27b2
RDDcat <yukmaro@gmail.com>
2023-03-23 16:17:46 +0900

    [UPDATE] 버그 수정 및 order-service 단일 마리아디비를 사용하도록 수정

--- order-service/src/main/java/com/maro/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/maro/orderservice/messagequeue/KafkaProducerConfig.java
@@ -1,6 +1,7 @@
 package com.maro.orderservice.messagequeue;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -18,9 +19,9 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory(){
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(properties);
     }
