2639292f7f5cea8d0214dbb36a5080446281d13c
RDDcat <yukmaro@gmail.com>
2023-03-26 07:18:49 +0900

    [UPDATE] micrometer, prometheus dependency, 설정추가

--- user-service/src/main/java/com/maro/userservice/controller/UserController.java
+++ user-service/src/main/java/com/maro/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.maro.userservice.service.UserService;
 import com.maro.userservice.vo.Greeting;
 import com.maro.userservice.vo.RequestUser;
 import com.maro.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,7 @@ public class UserController {
     }
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status(){
         return String.format("It's working on port"
                 +", port(local.server.port)="+ env.getProperty("local.server.port")
